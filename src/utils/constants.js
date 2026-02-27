f5b9e9b2d25197f647427aeb022df9fa6573811d
Eziio <thphong16902@gmail.com>
2026-02-27 17:43:24 +0700

    Fix: remove cross env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,1 @@
-let apiRoot = "";
-
-// môi trường dev
-if (process.env.BUILD_MODE === "dev") {
-  apiRoot = "http://localhost:8017";
-}
-// môi trường production
-if (process.env.BUILD_MODE === "production") {
-  apiRoot = "https://api.movie.eziio.site";
-}
-
-export const API_ROOT = apiRoot;
+export const API_ROOT = process.env.NEXT_PUBLIC_API_ROOT;
